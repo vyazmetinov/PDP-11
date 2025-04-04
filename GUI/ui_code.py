afcb0525f317e66bc2a0b3abdb7e98e42b50e9ec
vyazmetinov <138450265+vyazmetinov@users.noreply.github.com>
2025-04-04 15:25:12 +0300

    Commit #3
    Регистры и код лежат в QDockWidgets
    Вид в menubar

---
+++ GUI/ui_code.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'code.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QSizePolicy, QTextEdit,
+    QWidget)
+
+class Ui_Code(object):
+    def setupUi(self, Code):
+        if not Code.objectName():
+            Code.setObjectName(u"Code")
+        Code.resize(640, 480)
+        self.gridLayout = QGridLayout(Code)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.code = QTextEdit(Code)
+        self.code.setObjectName(u"code")
+        self.code.setAutoFormatting(QTextEdit.AutoNone)
+        self.code.setOverwriteMode(True)
+
+        self.gridLayout.addWidget(self.code, 0, 0, 1, 1)
+
+
+        self.retranslateUi(Code)
+
+        QMetaObject.connectSlotsByName(Code)
+    # setupUi
+
+    def retranslateUi(self, Code):
+        Code.setWindowTitle(QCoreApplication.translate("Code", u"Form", None))
+        self.code.setHtml(QCoreApplication.translate("Code", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'.AppleSystemUIFont'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">dslknflskn</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; mar"
+                        "gin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-bloc"
+                        "k-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\"-q"
+                        "t-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
+    # retranslateUi
+
---
+++ GUI/ui_mem_view.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mem_view.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDockWidget, QGridLayout, QHeaderView,
+    QLabel, QLineEdit, QSizePolicy, QTableView,
+    QWidget)
+
+class Ui_MemoryView(object):
+    def setupUi(self, MemoryView):
+        if not MemoryView.objectName():
+            MemoryView.setObjectName(u"MemoryView")
+        MemoryView.setEnabled(True)
+        MemoryView.resize(640, 480)
+        self.dockWidgetContents = QWidget()
+        self.dockWidgetContents.setObjectName(u"dockWidgetContents")
+        self.gridLayout_2 = QGridLayout(self.dockWidgetContents)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.table = QTableView(self.dockWidgetContents)
+        self.table.setObjectName(u"table")
+
+        self.gridLayout_2.addWidget(self.table, 2, 0, 1, 2)
+
+        self.from_item = QLineEdit(self.dockWidgetContents)
+        self.from_item.setObjectName(u"from_item")
+
+        self.gridLayout_2.addWidget(self.from_item, 0, 1, 1, 1)
+
+        self.from_label = QLabel(self.dockWidgetContents)
+        self.from_label.setObjectName(u"from_label")
+
+        self.gridLayout_2.addWidget(self.from_label, 0, 0, 1, 1)
+
+        self.to_label = QLabel(self.dockWidgetContents)
+        self.to_label.setObjectName(u"to_label")
+
+        self.gridLayout_2.addWidget(self.to_label, 1, 0, 1, 1)
+
+        self.to_item = QLineEdit(self.dockWidgetContents)
+        self.to_item.setObjectName(u"to_item")
+
+        self.gridLayout_2.addWidget(self.to_item, 1, 1, 1, 1)
+
+        MemoryView.setWidget(self.dockWidgetContents)
+
+        self.retranslateUi(MemoryView)
+
+        QMetaObject.connectSlotsByName(MemoryView)
+    # setupUi
+
+    def retranslateUi(self, MemoryView):
+        MemoryView.setWindowTitle(QCoreApplication.translate("MemoryView", u"\u041f\u0430\u043c\u044f\u0442\u044c", None))
+        self.from_label.setText(QCoreApplication.translate("MemoryView", u"\u041e\u0442", None))
+        self.to_label.setText(QCoreApplication.translate("MemoryView", u"\u0414\u043e", None))
+    # retranslateUi
+
