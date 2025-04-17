3a16772576d4caf28d1ad933af12ad3b2a35823e
Roman <romochka-trusov@dmc>
2025-04-18 00:45:21 +0300

    a

---
+++ GUI/code.py
@@ -0,0 +1,35 @@
+# This Python file uses the following encoding: utf-8
+import sys
+
+import PySide6
+from PySide6.QtCore import QAbstractItemModel, QAbstractTableModel, QModelIndex, Qt
+from PySide6.QtWidgets import QApplication, QWidget, QTableView
+import numpy as np
+
+import ui_code
+
+
+class MyModel_2(QAbstractTableModel):
+
+    def rowCount(self, parent: QModelIndex):
+        return 3
+    def columnCount(self, parent: QModelIndex):
+        return 2
+
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        if role == Qt.ItemDataRole.DisplayRole:
+            return index.row() + index.column()
+        return None
+    def headerData(self, col, orientation, role):
+        if role == Qt.ItemDataRole.DisplayRole and orientation == Qt.Orientation.Horizontal:
+            return ["Адрес", "Значение"][col]
+
+class Code(PySide6.QtWidgets.QWidget, ui_code.Ui_Code):
+    def __init__(self, parent: None):
+        print("Memory view.init")
+        super().__init__(parent)
+        self.setupUi(self)
+        print("setupUI")
+        self.model = MyModel_2()
+
+        self.ui = ui_code.Ui_Code()
